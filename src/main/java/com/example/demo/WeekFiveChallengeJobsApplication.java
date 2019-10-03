052e45426f51ccd3f9daca85794aeea9352d081b
fdmckie <fdmckie@gmail.com>
2019-10-03 16:39:40 -0400

    Week Five Challenge; Craigslist Job Posting

---
+++ src/main/java/com/example/demo/WeekFiveChallengeJobsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekFiveChallengeJobsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeekFiveChallengeJobsApplication.class, args);
+    }
+
+}
