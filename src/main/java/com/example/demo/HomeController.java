63a2cc97df09a36e8c900296793cf7902946b906
fdmckie <fdmckie@gmail.com>
2019-10-04 14:24:16 -0400

    Week 5 Challenge: Craigslist Job Posting

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,13 +1,13 @@
 package com.example.demo;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.stereotype.Controller;
+        import org.springframework.ui.Model;
+        import org.springframework.validation.BindingResult;
+        import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+        import javax.validation.Valid;
 
 @Controller
 public class HomeController {
